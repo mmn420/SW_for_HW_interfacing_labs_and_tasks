716f33acc2b942c0d05698d83391bbecebe7c96b
Mo'men Maged <momen99223@gmail.com>
2021-12-23 09:52:06 +0200

    added lab 3 again

--- lab3/Core/Src/main.c
+++ lab3/Core/Src/main.c
@@ -1,6 +1,23 @@
 #include "stm32f401cc_interface.h"
 
-int main(void) {
-  while (1) {}
-  return 0;
+void delay(int seconds)
+{
+    while (seconds--)
+    {
+    }
+}
+
+int main(void)
+{
+    RCC_AHB1ENR |= (0x01 << 0);
+    GPIOA_MODER |= (0x01 << 0);
+    GPIOA_OTYPER &= ~(0x01 << 0);
+    while (1)
+    {
+        GPIOA_ODR &= ~(1 << 0);
+        delay(1000000);
+        GPIOA_ODR |= (1 << 0);
+        delay(1000000);
+    }
+    return 0;
 }
